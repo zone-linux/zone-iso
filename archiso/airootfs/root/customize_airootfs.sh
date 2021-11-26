edf865b3ff4410ef7d8f8aa0979b0b1cf29fdd88
Kungger-git <matthewriel63@gmail.com>
2021-11-27 06:34:13 +0800

    Update airootfs script for modifying lightdm cursor theme

--- archiso/airootfs/root/customize_airootfs.sh
+++ archiso/airootfs/root/customize_airootfs.sh
@@ -11,3 +11,8 @@ for dfile in "${rootdots[@]}"; do if [[ -e "$skeldir/$dfile" ]]; then cp -rf "$s
 
 declare -a rootconfig=(xfce4 autostart dconf dunst ulauncher geany gtk-3.0 Kvantum neofetch nvim qt5ct ranger Thunar)
 for cfg in "${rootconfig[@]}"; do if [[ -e "$skeldir/.config/$cfg" ]]; then cp -rf "$skeldir"/.config/"$cfg" "$rootdir"; fi done
+
+# Replacing lightdm cursor theme
+IND_THEME="/usr/share/icons/default/index.theme"
+echo "[Icon Theme]" > "$IND_THEME"
+echo "Inherits=Zone" >> "$IND_THEME"
