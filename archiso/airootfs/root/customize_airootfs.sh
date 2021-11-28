69deb5bd49bfdcee9426e40d15efaf32ac37d2e3
Kungger-git <matthewriel63@gmail.com>
2021-11-28 20:21:56 +0800

    Remove .gtkrc-2.0 to copying over to root

--- archiso/airootfs/root/customize_airootfs.sh
+++ archiso/airootfs/root/customize_airootfs.sh
@@ -6,7 +6,7 @@ skeldir="/etc/skel"
 rootdir="/root/.config"
 if [[ ! -d "$rootdir" ]]; then mkdir -p "$rootdir"; fi
 
-declare -a rootdots=('.local' '.gtkrc-2.0' '.vim_runtime' '.vimrc')
+declare -a rootdots=('.local' '.vim_runtime' '.vimrc')
 for dfile in "${rootdots[@]}"; do if [[ -e "$skeldir/$dfile" ]]; then cp -rf "$skeldir"/"$dfile" /root; fi done
 
 declare -a rootconfig=(xfce4 autostart dconf dunst ulauncher geany gtk-3.0 Kvantum neofetch nvim qt5ct ranger Thunar)
