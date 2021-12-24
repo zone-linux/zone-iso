2e5eee872f3ed1c6d5a2d35114566966f18cef63
Kungger-git <matthewriel63@gmail.com>
2021-12-24 12:50:29 +0800

    Update custom rootfs

--- archiso/airootfs/root/customize_airootfs.sh
+++ archiso/airootfs/root/customize_airootfs.sh
@@ -3,14 +3,14 @@ set -e -u
 
 ## Copy Specific Configurations to Root Directory
 skeldir="/etc/skel"
-rootdir="/root/.config"
-if [[ ! -d "$rootdir" ]]; then mkdir -p "$rootdir"; fi
+rootdir="/root"
+if [[ ! -d "$rootdir"/.config ]]; then mkdir -p "$rootdir"/.config; fi
 
-declare -a rootdots=('.local' '.vim_runtime' '.vimrc')
+declare -a rootdots=('.local' '.vim_runtime' '.vimrc' '.mpd' '.dmrc' '.zshrc' '.zshrc-personal')
 for dfile in "${rootdots[@]}"; do if [[ -e "$skeldir/$dfile" ]]; then cp -rf "$skeldir"/"$dfile" /root; fi done
 
-declare -a rootconfig=(xfce4 autostart dconf dunst ulauncher geany gtk-3.0 Kvantum neofetch nvim qt5ct ranger Thunar)
-for cfg in "${rootconfig[@]}"; do if [[ -e "$skeldir/.config/$cfg" ]]; then cp -rf "$skeldir"/.config/"$cfg" "$rootdir"; fi done
+declare -a rootconfig=(dconf cava qt5ct neofetch conky Thunar gtk-2.0 gtk-3.0 xfce4 geany Kvantum ulauncher ranger autostart)
+for cfg in "${rootconfig[@]}"; do if [[ -e "$skeldir/.config/$cfg" ]]; then cp -rf "$skeldir"/.config/"$cfg" "$rootdir"/.config; fi done
 
 # Replacing lightdm cursor theme
 IND_THEME="/usr/share/icons/default/index.theme"
