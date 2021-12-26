dfe702af38cf88d4a545b23249d18b6936ccd332
Kungger-git <matthewriel63@gmail.com>
2021-12-26 23:05:13 +0800

    Update customize root fs

--- archiso/airootfs/root/customize_airootfs.sh
+++ archiso/airootfs/root/customize_airootfs.sh
@@ -9,7 +9,7 @@ if [[ ! -d "$rootdir"/.config ]]; then mkdir -p "$rootdir"/.config; fi
 declare -a rootdots=('.local' '.vim_runtime' '.vimrc' '.mpd' '.dmrc' '.zshrc' '.zshrc-personal')
 for dfile in "${rootdots[@]}"; do if [[ -e "$skeldir/$dfile" ]]; then cp -rf "$skeldir"/"$dfile" /root; fi done
 
-declare -a rootconfig=(dconf cava qt5ct neofetch conky Thunar gtk-2.0 gtk-3.0 xfce4 geany Kvantum ulauncher ranger autostart)
+declare -a rootconfig=(dconf cava qt5ct neofetch conky Thunar gtk-2.0 gtk-3.0 xfce4 geany Kvantum ulauncher ranger autostart nvim)
 for cfg in "${rootconfig[@]}"; do if [[ -e "$skeldir/.config/$cfg" ]]; then cp -rf "$skeldir"/.config/"$cfg" "$rootdir"/.config; fi done
 
 # Replacing lightdm cursor theme
